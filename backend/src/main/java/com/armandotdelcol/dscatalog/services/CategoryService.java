39eade4eb94c3bd3990e1b1cae581753325df81e
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-01-27 13:56:40 -0300

    Find Category By Id

--- backend/src/main/java/com/armandotdelcol/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/armandotdelcol/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,6 +22,13 @@ public class CategoryService {
         List<Category> list = categoryRepository.findAll();
         List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
         return listDto;
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
     }
 
 }
