9dc97b4677d4f89edd097a7648e025d344421b21
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-01-27 12:16:12 -0300

    database H2, layers

--- backend/src/main/java/com/armandotdelcol/dscatalog/entities/Category.java
+++ backend/src/main/java/com/armandotdelcol/dscatalog/entities/Category.java
@@ -1,11 +1,17 @@
 package com.armandotdelcol.dscatalog.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Category() {
